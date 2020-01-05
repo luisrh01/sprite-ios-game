068334eed5b32dcfb5134086646f94524d62cf1d
Luis Hernandez <luisrh@mac.com>
2020-01-05 15:45:29 -0500

    Initial Commit

---
+++ Snake/GameManager.swift
@@ -0,0 +1,12 @@
+//
+//  GameManager.swift
+//  Snake
+//
+//  Created by Luis Hernandez on 1/4/20.
+//  Copyright © 2020 BPS Consulting. All rights reserved.
+//
+
+import SpriteKit
+class GameManager {
+
+}
